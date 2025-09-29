ec6daa48fd87d7ace191e467118f9e98ca40a6fc
iovsomnium <ksw09157.5212@gmail.com>
2025-09-29 12:17:23 +0900

    refactor: Add cors multi-settings

--- services/payment/internal/config/service.go
+++ services/payment/internal/config/service.go
@@ -5,11 +5,29 @@ type ServiceConfig struct {
 	Environment         string         `yaml:"environment"`
 	Version             string         `yaml:"version"`
 	ClientURL           string         `yaml:"client_url"`
+	ClientURLs          []string       `yaml:"client_urls"`
 	StripeSecretKey     string         `yaml:"stripe_secret_key"`
 	StripeWebhookSecret string         `yaml:"stripe_webhook_secret"`
 	EnableTestEndpoints bool           `yaml:"enable_test_endpoints"`
 	Supabase            SupabaseConfig `yaml:"supabase"`
 	Toss                TossConfig     `yaml:"toss"`
+}
+
+func (s *ServiceConfig) AllowedClientOrigins() []string {
+	if len(s.ClientURLs) > 0 {
+		return append([]string(nil), s.ClientURLs...)
+	}
+	if s.ClientURL != "" {
+		return []string{s.ClientURL}
+	}
+	return []string{}
+}
+
+func (s *ServiceConfig) PrimaryClientURL() string {
+	if len(s.ClientURLs) > 0 {
+		return s.ClientURLs[0]
+	}
+	return s.ClientURL
 }
 
 type SupabaseConfig struct {
